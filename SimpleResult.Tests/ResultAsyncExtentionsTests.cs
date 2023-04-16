5761a965a7444232db41235d0e6bd4c9bd542ecc
meisamalifallahi@gmail.com <meisamalifallahi@gmail.com>
2023-04-16 12:44:51 +0200

    Add Async support

---
+++ SimpleResult.Tests/ResultAsyncExtentionsTests.cs
@@ -0,0 +1,271 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace SimpleResult.Tests;
+
+public class ResultAsyncExtensionsTests
+{
+      private static async Task<string> SuccessAsyncOperation()
+    {
+        await Task.Delay(50);
+        return "Success";
+    }
+
+    private static async Task<string> FailureAsyncOperation()
+    {
+        await Task.Delay(50);
+        throw new InvalidOperationException("Failure");
+    }
+
+    [Fact]
+    [Trait("Category", "OnSuccessAsync")]
+    public async Task OnSuccessAsync_ShouldExecuteAction_WhenResultIsSuccess()
+    {
+        var result = Result<string>.Success("Success");
+        bool executed = false;
+
+        await result.OnSuccess(async _ =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("Category", "OnSuccessAsync")]
+    public async Task OnSuccessAsync_ShouldNotExecuteAction_WhenResultIsFailure()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+        bool executed = false;
+
+        await result.OnSuccess(async _ =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("Category", "OnFailureAsync")]
+    public async Task OnFailureAsync_ShouldExecuteAction_WhenResultIsFailure()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+        bool executed = false;
+
+        await result.OnFailure(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("Category", "OnFailureAsync")]
+    public async Task OnFailureAsync_ShouldNotExecuteAction_WhenResultIsSuccess()
+    {
+        var result = Result<string>.Success("Success");
+        bool executed = false;
+
+        await result.OnFailure(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeFalse();
+    }
+    [Fact]
+    [Trait("Category", "OnExceptionAsync")]
+    public async Task OnExceptionAsync_ShouldExecuteAction_WhenResultHasException()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+        bool executed = false;
+
+        await result.OnException<InvalidOperationException>(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("Category", "OnExceptionAsync")]
+    public async Task OnExceptionAsync_ShouldNotExecuteAction_WhenResultHasNoException()
+    {
+        var result = Result<string>.Success("Success");
+        bool executed = false;
+
+        await result.OnException<InvalidOperationException>(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("Category", "OnInnerExceptionAsync")]
+    public async Task OnInnerExceptionAsync_ShouldExecuteAction_WhenResultHasInnerException()
+    {
+        var innerException = new InvalidOperationException("Inner Failure");
+        var outerException = new ApplicationException("Outer Failure", innerException);
+        var result = Result<string>.Fail(outerException);
+        bool executed = false;
+
+        await result.OnInnerException<InvalidOperationException>(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("Category", "OnInnerExceptionAsync")]
+    public async Task OnInnerExceptionAsync_ShouldNotExecuteAction_WhenResultHasNoInnerException()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+        bool executed = false;
+
+        await result.OnInnerException<InvalidOperationException>(async (ex, errors) =>
+        {
+            executed = true;
+            await Task.CompletedTask;
+        });
+
+        executed.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("Category", "SwitchAsync")]
+    public async Task SwitchAsync_ShouldExecuteOnSuccessAction_WhenResultIsSuccess()
+    {
+        var result = Result<string>.Success("Success");
+        bool onSuccessExecuted = false;
+        bool onFailureExecuted = false;
+
+        await result.Switch(
+            async () =>
+            {
+                onSuccessExecuted = true;
+                await Task.CompletedTask;
+            },
+            async (ex, errors) =>
+            {
+                onFailureExecuted = true;
+                await Task.CompletedTask;
+            });
+
+        onSuccessExecuted.Should().BeTrue();
+        onFailureExecuted.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("Category", "SwitchAsync")]
+    public async Task SwitchAsync_ShouldExecuteOnFailureAction_WhenResultIsFailure()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+        bool onSuccessExecuted = false;
+        bool onFailureExecuted = false;
+
+        await result.Switch(
+            async () =>
+            {
+                onSuccessExecuted = true;
+                await Task.CompletedTask;
+            },
+            async (ex, errors) =>
+            {
+                onFailureExecuted = true;
+                await Task.CompletedTask;
+            });
+
+        onSuccessExecuted.Should().BeFalse();
+        onFailureExecuted.Should().BeTrue();
+    }
+    [Fact]
+    [Trait("Category", "FoldAsync")]
+    public async Task FoldAsync_ShouldExecuteOnSuccessFunc_WhenResultIsSuccess()
+    {
+        var result = Result<string>.Success("Success");
+
+        string returnedValue = await result.Fold(
+            async () =>
+            {
+                await Task.CompletedTask;
+                return "OnSuccess";
+            },
+            async (ex, errors) =>
+            {
+                await Task.CompletedTask;
+                return "OnFailure";
+            });
+
+        returnedValue.Should().Be("OnSuccess");
+    }
+
+    [Fact]
+    [Trait("Category", "FoldAsync")]
+    public async Task FoldAsync_ShouldExecuteOnFailureFunc_WhenResultIsFailure()
+    {
+        var result = Result<string>.Fail(new InvalidOperationException("Failure"));
+
+        string returnedValue = await result.Fold(
+            async () =>
+            {
+                await Task.CompletedTask;
+                return "OnSuccess";
+            },
+            async (ex, errors) =>
+            {
+                await Task.CompletedTask;
+                return "OnFailure";
+            });
+
+        returnedValue.Should().Be("OnFailure");
+    }
+
+    [Fact]
+    [Trait("Category", "MapAsync")]
+    public async Task MapAsync_ShouldTransformResultValue_WhenResultIsSuccess()
+    {
+        var result = Result<int>.Success(2);
+
+        var newResult = await result.Map(async value =>
+        {
+            await Task.CompletedTask;
+            return value * 2;
+        });
+
+        newResult.IsSuccess.Should().BeTrue();
+        newResult.GetOrDefault().Should().Be(4);
+    }
+
+    [Fact]
+    [Trait("Category", "MapAsync")]
+    public async Task MapAsync_ShouldNotTransformResultValue_WhenResultIsFailure()
+    {
+        var result = Result<int>.Fail(new InvalidOperationException("Failure"));
+
+        var newResult = await result.Map(async value =>
+        {
+            await Task.CompletedTask;
+            return value * 2;
+        });
+
+        newResult.IsFailure.Should().BeTrue();
+    }
+}
+
---
+++ SimpleResult/ResultAsyncExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace SimpleResult;
+
+public static class ResultAsyncExtensions
+{
+    public static async Task<IResult> OnSuccess(this IResult result, Func<Task> action)
+    {
+        if (result.IsSuccess) await action();
+        return result;
+    }
+    public static async Task<IResult<T>> OnSuccess<T>(this IResult<T> result, Func<T, Task> action)
+    {
+        if (result.IsSuccess) await action(result.GetOrDefault());
+        return result;
+    }
+    public static async Task<IResult> OnFailure(this IResult result, Func<Exception?, Errors, Task> action)
+    {
+
+        if (result.IsFailure) await action(result.ExceptionOrNull(), result.Errors);
+        return result;
+    }
+    public static async Task<IResult> OnFailure<TError>(this IResult result, Func<Exception?, IReadOnlyCollection<TError>, Task> action) where TError : IError
+    {
+        if (!result.IsFailure) return result;
+        if (result.Errors.OfType<TError>().Any())
+        {
+            await action(result.ExceptionOrNull(), result.Errors.OfType<TError>().ToList());
+        }
+        return result;
+    }
+    public static async Task<IResult> OnException(this IResult result, Func<Exception, Errors, Task> action)
+    {
+        var exception = result.ExceptionOrNull();
+        if (exception is not null) await action(exception, result.Errors);
+        return result;
+    }
+    public static async Task<IResult> OnException<T>(this IResult result, Func<T, Errors, Task> action) where T : Exception
+    {
+        var exception = result.ExceptionOrNull();
+        if (exception is T ex) await action(ex, result.Errors);
+        return result;
+    }
+    public static async Task<IResult> OnInnerException<T>(this IResult result, Func<T, Errors, Task> action) where T : Exception
+    {
+        var exception = result.ExceptionOrNull();
+        if (exception?.InnerException is T ex) await action(ex, result.Errors);
+        return result;
+    }
+    public static async Task Switch<TResult>(this TResult result, Func<Task> onSuccess, Func<Exception?, Errors, Task> onFailure) where TResult : IResult
+    {
+        if (result.IsSuccess)
+        {
+            await onSuccess();
+        }
+        else
+        {
+            await onFailure(result.ExceptionOrNull(), result.Errors);
+        }
+    }
+    public static async Task Switch<T>(this IResult<T> result, Func<T, Task> onSuccess, Func<Exception?, Errors, Task> onFailure)
+    {
+        if (result.IsSuccess)
+        {
+            await onSuccess(result.GetOrDefault());
+        }
+        else
+        {
+            await onFailure(result.ExceptionOrNull(), result.Errors);
+        }
+    }
+    public static async Task<TReturn> Fold<TReturn, TResult>(this TResult result, Func<Task<TReturn>> onSuccess, Func<Exception?, Errors, Task<TReturn>> onFailure) where TResult : IResult
+    {
+        return result.IsSuccess
+            ? await onSuccess()
+            : await onFailure(result.ExceptionOrNull(), result.Errors);
+    }
+    public static async Task<TReturn> Fold<TReturn, T>(this IResult<T> result, Func<T, Task<TReturn>> onSuccess, Func<Exception?,Errors, Task<TReturn>> onFailure)
+    {
+        return result.IsSuccess 
+            ? await onSuccess(result.GetOrDefault()) 
+            : await onFailure(result.ExceptionOrNull(), result.Errors);
+    }
+    public static async Task<Result<TR>> Map<TL, TR>(this Result<TL> result, Func<TL, Task<TR>> transform)
+    {
+        return result.IsSuccess 
+            ? Result<TR>.Success(await transform(result.GetOrDefault())) 
+            : Result<TR>.Fail(result.ExceptionOrNull(), result.Errors);
+    }
+}
